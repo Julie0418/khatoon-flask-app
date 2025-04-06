5653a14a17ffb5505429c210c3e9102e254a7db6
Julie0418 <khatoonjulekha4@gmail.com>
2025-04-06 11:11:31 -0400

    updated app

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_cloud():
-    return 'Updated Hello Cloud!'
+    return 'Hello from khatoon ECS Container!'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
