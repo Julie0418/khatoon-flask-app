fc4bd990d4e3f24d7d2453f991aa32180b518eae
Julie0418 <khatoonjulekha4@gmail.com>
2025-04-07 00:02:13 -0400

    added terraform

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello_cloud():
     return 'Hello from khatoon ECS Container!'
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
