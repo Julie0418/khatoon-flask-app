60a812522a0f3939f3e506a141962c89d98fe478
Julie0418 <khatoonjulekha4@gmail.com>
2025-04-10 13:20:39 -0400

    updated

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+
+from flask import Flask, jsonify
+import socket
+
 app = Flask(__name__)
+
+# Get hostname and IP address
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+# Main route modified as per assignment requirements
 @app.route('/')
-def hello_cloud():
-    return 'Hello from khatoon ECS Container!'
+def home():
+    return jsonify({"message": "Welcome to Khatoon Final Test API Server"})
+
+# Host route
+@app.route('/host')
+def host_name():
+    return jsonify({"hostname": hostname})
+
+# IP route
+@app.route('/ip')
+def host_ip():
+    return jsonify({"ip_address": ip_address})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
